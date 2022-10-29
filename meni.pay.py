4fbce243ed5cc5e7df807f3b92462eda83d1fd6a
student <mm29082009mm@gmail.com>
2022-10-29 13:19:04 +0300

    jh

---
+++ meni.pay.py
@@ -0,0 +1,9 @@
+ import random
+class Human:
+    def __init__(self, name="blaua", job = None , home = None, car = None):
+    self.name = name
+    self.job = job
+    self.home = home
+    self.car = car
+    self.gladess = 50
+    self.stiety = 50
